9a24b46d0e8356360c6baafefa421af5f5d22a40
Vishal-Borse <vishalsborse8@gmail.com>
2024-12-11 14:48:38 +0530

    Created starter react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-sky-400/100 text-center">ReadMoree.in</h1>
+    </div>
+  );
+}
+
+export default App;
