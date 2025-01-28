0cbe8cfebd7230c4d0b7c520c4b62c6d5f0dc2a3
Vivek Kolhe <vivekkolhe694@gmail.com>
2025-01-28 20:08:43 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import Dropdown from "./Components/Dropdown";
-import CategorySearch from "./Pages/CategorySearch";
-import ProductDes from "./Components/ProductDes";
-import BookComponent from "./Screens/BookComponent";
 import HomePage from "./Pages/HomePage";
 
 function App() {
